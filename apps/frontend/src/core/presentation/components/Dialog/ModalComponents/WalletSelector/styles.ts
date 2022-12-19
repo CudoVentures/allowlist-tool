6acf9d4c19366cbee08196563d70275fc3098ee9
SpaghettiOverload <imyours@mail.bg>
2022-12-19 16:17:19 +0200

    adjust log in modal according the designs

--- apps/frontend/src/core/presentation/components/Dialog/ModalComponents/WalletSelector/styles.ts
+++ apps/frontend/src/core/presentation/components/Dialog/ModalComponents/WalletSelector/styles.ts
@@ -17,7 +17,7 @@ export const styles = {
     },
     connectButton: {
         height: '50px',
-        width: '250px',
+        width: '100%'
     },
     btnsHolder: {
         alignItems: 'center',
