481ca169092604569ca21a69bd52d9917269330d
mlukanova <meglena.lukanova@gmail.com>
2023-01-04 13:45:56 +0200

    Add port configuration for the db

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -29,7 +29,7 @@ import { UserController } from './user/user.controller';
         return {
           dialect: 'postgres',
           host: config.get('APP_DATABASE_HOST'),
-          port: 5434,
+          port: config.get('APP_DATABASE_PORT'),
           username: config.get('APP_DATABASE_USER'),
           password: config.get('APP_DATABASE_PASS'),
           database: config.get('APP_DATABASE_DB_NAME'),
