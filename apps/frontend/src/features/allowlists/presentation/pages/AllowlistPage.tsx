2eedea4e8dbdd7e24811f80148c8955bb6a1f22f
mlukanova <meglena.lukanova@gmail.com>
2023-01-24 13:59:28 +0200

    Fix allowlist details page

--- apps/frontend/src/features/allowlists/presentation/pages/AllowlistPage.tsx
+++ apps/frontend/src/features/allowlists/presentation/pages/AllowlistPage.tsx
@@ -1,54 +1,48 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { Circles as CirclesSpinner } from 'svg-loaders-react';
-import { Box } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { FetchedAllowlist } from '../../../../core/store/allowlist';
-import { GET_ALL_ALLOWLISTS } from '../../../../core/api/calls';
+import Allowlist from '../components/Allowlist';
 import NoResult from '../../../../core/presentation/components/Layout/NoResult';
-import { COLORS_DARK_THEME } from '../../../../core/theme/colors';
-import CreatedAllowlistsPreview from './CreatedAllowlists';
-import JoinedAllowlistsPreview from './JoinedAllowlists';
+import { FetchedAllowlist } from '../../../../core/store/allowlist';
+import { GET_ALLOWLIST_DETAILS } from '../../../../core/api/calls';
+import { StyledCircleSpinner } from '../../../../core/presentation/components/Layout/helpers';
 
-import { generalStyles } from './styles';
+function AllowlistPage() {
 
-const AllAllowlistsPage = () => {
-
-  const [allowlists, setAllowlists] = useState<FetchedAllowlist[]>([])
+  const { id } = useParams();
+  const [allowlist, setAllowlist] = useState<FetchedAllowlist>(null);
   const [loading, setLoading] = useState<boolean>(true)
 
   const contentHandler = useCallback((): JSX.Element => {
     if (loading) {
-      return <CirclesSpinner style={generalStyles.spinner}
-        fill={COLORS_DARK_THEME.PRIMARY_BLUE} />
+      return <StyledCircleSpinner />
     }
-
-    if (allowlists.length) {
-      return (
-        <Box id='swipersHolder' gap={4} sx={generalStyles.swipersHolder}>
-          <CreatedAllowlistsPreview data={allowlists} />
-          <JoinedAllowlistsPreview data={allowlists} />
-        </Box>
-      )
+    if (Object.keys(allowlist || {}).length) {
+      return <Allowlist props={{ ...allowlist, end_date: new Date(allowlist.end_date) }} />
     }
-
     return <NoResult />
-  }, [loading, allowlists])
-
-  const loadData = async () => {
-    try {
-      const data = await GET_ALL_ALLOWLISTS()
-      setAllowlists(data)
-
-    } finally {
-      setLoading(false)
-    }
-  }
+  }, [loading, allowlist])
 
   useEffect(() => {
-    loadData()
-  }, [])
+    const setAllowlistDetails = async () => {
+
+      try {
+        const res = await GET_ALLOWLIST_DETAILS(id)
+        const data = res.data;
+        delete data.createdAt;
+        delete data.updatedAt;
+        setAllowlist(data);
+
+      } catch (error) {
+        console.error(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    setAllowlistDetails()
+  }, []);
 
   return contentHandler()
 }
 
-export default AllAllowlistsPage
+export default AllowlistPage;
