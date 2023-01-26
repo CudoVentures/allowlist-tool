f0ccc78efd5b398970d44404d1a91525567e56dc
mlukanova <meglena.lukanova@gmail.com>
2023-01-26 09:54:44 +0200

    Build fixes

--- apps/frontend/src/features/allowlists/presentation/pages/my-allowlists.tsx
+++ apps/frontend/src/features/allowlists/presentation/pages/my-allowlists.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import Allowlist from '../components/allowlist';
+import Allowlist from '../components/Allowlist';
 
 function MyAllowlistsPage({ walletStore }) {
   const [allowlists, setAllowlists] = useState([]);
